654769efa268b29748dc7eb37c5bc0a314ebfab1
Pranav Lawate <pran.lawate@gmail.com>
2026-02-22 11:15:51 +0530

    fix: remove extra comment line from kettlebell exercises to match spec
    Removed `// Kettlebell exercises` comment that was not in the
    specification for Task 1 of kettlebell integration.

--- js/modules/equipment-profiles.js
+++ js/modules/equipment-profiles.js
@@ -43,7 +43,6 @@ export const EQUIPMENT_REQUIREMENTS = {
   'Barbell Deadlift': ['barbells'],
   'Barbell Overhead Press': ['barbells'],
 
-  // Kettlebell exercises
   'KB Goblet Squat': ['kettlebells'],
   'KB Swings': ['kettlebells'],
 
